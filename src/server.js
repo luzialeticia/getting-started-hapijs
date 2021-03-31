461903614dfb682d68d6ab18498a17814b29589c
Letícia Furtado <leticialuzia@icloud.com>
2021-03-30 22:52:32 -0300

    Creating the router and starting server

--- src/server.js
+++ src/server.js
@@ -5,3 +5,20 @@ const server = new Hapi.Server();
 server.connection({
     port: 8080
 });
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: ( request, reply ) => {
+        reply('Hello World!');
+    }
+})
+
+server.start(err => {
+    if(err) {
+        console.log('Error was handled!');
+        console.log(err);
+    }
+
+    console.log(`Server started at ${server.info.uri}`);
+});
